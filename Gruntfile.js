e972a48c89f8092792d0a542b00bc9aa5a965c5c
skrimbleshanks <timwbodine@gmail.com>
2020-03-17 14:14:22 -0400

    add grunt-bake for html templating

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,23 @@ module.exports = function(grunt) {
                 livereload: 35729
               }
             },
+						bake: {
+							files: ["html/**"],
+							tasks: "bake:build"
+						},
             all: { files: ['**/*.html'], options: { livereload: true } } 
         },
-        sass: { dev: { files: { 'css/main.css':'sass/main.sass' } } }
+        sass: { dev: { files: { 'css/main.css':'sass/main.sass' } } },
+				bake: {
+					build: {
+							files: {
+								"html/index.html" : "html/base.html"
+							}
+					}
+				}
+
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks( "grunt-bake" );
 };
