1361c646954a1dc49cc1b9313831af87f4ba1d36
skrimbleshanks <timwbodine@gmail.com>
2020-03-17 14:27:26 -0400

    fix looping Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,30 @@
 module.exports = function(grunt) { 
-    grunt.initConfig({
-        watch: {
-            sass: {
-              files: '**/*.sass',
-              tasks: ['sass'],
-              options: {
-                livereload: 35729
-              }
-            },
-						bake: {
-							files: ["html/**"],
-							tasks: "bake:build"
-						},
-            all: { files: ['**/*.html'], options: { livereload: true } } 
-        },
-        sass: { dev: { files: { 'css/main.css':'sass/main.sass' } } },
-				bake: {
-					build: {
-							files: {
-								"html/index.html" : "html/base.html"
-							}
-					}
-				}
+  grunt.initConfig({
+    watch: {
+      sass: {
+        files: '**/*.sass',
+        tasks: ['sass'],
+        options: {
+        livereload: 35729
+        }
+      },
+      bake: {
+        files: ["html/**"],
+        tasks: "bake:build"
+      },
+      all: { files: ['**/*.html'], options: { livereload: true } } 
+    },
+    sass: { dev: { files: { 'css/main.css':'sass/main.sass' } } },
+    bake: {
+      build: {
+        files: {
+        "html/index.html" : "html/base.html"
+        }
+      }
+    }
 
-    });
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-		grunt.loadNpmTasks( "grunt-bake" );
+  });
+  grunt.loadNpmTasks( "grunt-bake" );
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 };
